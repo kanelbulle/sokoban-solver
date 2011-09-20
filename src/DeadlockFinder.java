ee9dc02c5885a1aef94c29dcae0364645883e9c5
Sajjon <alex.cyon@gmail.com>
2011-09-20 14:18:32 +0200

    Worked further on the methods for finding deadlocks

--- src/DeadlockFinder.java
+++ src/DeadlockFinder.java
@@ -2,32 +2,6 @@ import java.util.ArrayList;
 
 public class DeadlockFinder {
 
-	/* The following will be replaced by the real variables, provided by Emil */
-	//begin temporary
-	byte IS_BOX = 0x23;
-	byte IS_GOAL = 0x24;
-	byte IS_FLOOR = 0x25;
-	byte IS_BOX_ON_GOAL = 0x26;
-	byte IS_WALL = 0x27;
-	byte IS_PLAYER = 0x28;
-	byte IS_NOTHING = 0x29;
-	//end temporary
-	
-	private class Board {
-		byte atPos(Pos currPos) {
-			return 0x11;
-		}
-	};
-	
-	private class Pos{
-		int row;
-		int col;
-		public Pos(int row, int col) {
-			this.row = row;
-			this.col = col;
-		}
-	};
-
 	/**
 	 * This method receives an instance of class Board and for that Board
 	 * determines whether it is a deadlocked board or not, i.e. if it is
@@ -56,108 +30,221 @@ public class DeadlockFinder {
 	 * Freeze deadlock when being located on a goal. Nevertheless they may
 	 * influence the reachable area of other boxes.
 	 * 
-	 * @param Board to determine if deadlocked or not
+	 * @param Board
+	 *            to determine if deadlocked or not
 	 * @return Returns "True" if the board is deadlocked.
 	 */
-	public boolean isDeadLock(Board pBoard) {
-		return (isDSAFD(pBoard) || isCD(pBoard) || isBD(pBoard) || isDDTFB(pBoard));
+	public boolean isDeadLock(Board theBoard) {
+		return (isDSAFD(theBoard) || isCD(theBoard) || isBD(theBoard) || isDDTFB(theBoard));
 	}
-	
-	
+
 	/**
-	 * This is an essential help method for at least isDSD (help method to isDeadLock).
-	 * @param Pos is the position of the box to check if deadlocked.
+	 * This is an essential help method for at least isDSD (help method to
+	 * isDeadLock).
+	 * 
+	 * @param Pos
+	 *            is the position of the box to check if deadlocked.
 	 * @return Returns TRUE if the box is immovable
 	 */
-	private boolean isBoxImmovable(Pos boxPos) {
+	private boolean isBoxImmovable(Board theBoard, BoardCoordinate boxPos) {
+		byte dataNorth, dataSouth, dataEast, dataWest;
+		dataNorth = theBoard.dataAt((byte) (boxPos.row - 1), boxPos.column);
+		dataSouth = theBoard.dataAt((byte) (boxPos.row + 1), boxPos.column);
+		dataEast = theBoard.dataAt(boxPos.row, (byte) (boxPos.column + 1));
+		dataWest = theBoard.dataAt(boxPos.row, (byte) (boxPos.column - 1));
+
+		// @formatter:off
+		// 9 O'Clock
+		// 							  # <- non-reachable square
+		// non-reachable square -> # [] <- Box
+		boolean nineOclock = (!isReachable(dataNorth) && !isReachable(dataWest));
+
+		// 3 O'Clock
+		// 		   # <-- non-reachable square
+		// box -> [] # <-- non-reachable square
+		boolean threeOclock = (!isReachable(dataNorth) && !isReachable(dataEast));
+
+		// Quarter past six
+		// box -> [] # <-- non-reachable square
+		// 		   # <-- non-reachable square
+		boolean qPastSix = (!isReachable(dataSouth) && !isReachable(dataEast));
+
 		
-		int freeAdjacentPosCount = 0;
-		
-		//TODO do this!
-		
+		// Quarter to six
+		// non-reachable square -> # [] <- Box
+		// 							  # <- non-reachable square
+		// @formatter:on
+		boolean qToSix = (!isReachable(dataSouth) && !isReachable(dataWest));
+
+		// check if box is in a corner and therefore immovable
+		if (nineOclock || threeOclock || qPastSix || qToSix) {
+			return true; // return, because box is in corner
+		} else {
+			// Check if box is stuck against a wall next to which there are no
+			// goals.
+			
+			//box is not totally free
+			if(!isReachable(dataNorth) || !isReachable(dataSouth) || !isReachable(dataEast) || !isReachable(dataWest)) {
+				
+			}
+			return false; // TODO remove this
+		}
+	}
+
+	/**
+	 * This method determines if the box is "in a bowl", that means that the box
+	 * is stuck against local "wall" that is closed off on the sides, so that
+	 * the box can't be moved from that wall, or stuck against a outer "wall".
+	 * 
+	 * We use the word "wall" since the wall can be either a wall of
+	 * Board.TYPE_WALL or boxes or boxes on goals.
+	 * 
+	 * Here is a few examples of situations where the box is "in a bowl":
+	 * Legend: # = "wall", [] = box, X = goal
+	 */
+	// @formatter:off
+	 // 
+	 //  1) WestBowl		2) NorthBowl		3)	SouthBowl	4) EastBowl
+	 // 
+	 // 	##				#########		    #	X	 #		   	   ##
+	 // 	#				#	[]	#			#	[]	 #				#
+	 // 	#  X			  X					##########				#
+	 // 	#[]														  []#
+	 // 	#														X	#
+	 // 	##														   ##
+	 // 
+	 // @formatter:on 
+	/**
+	 * @param theBoard
+	 * @param boxPos
+	 * @return
+	 */
+	private boolean inBowl(Board theBoard, BoardCoordinate boxPos) {
+		//TODO is this necessary? Do in isBoxImmovable instead?
 		return true;
 	}
 
 	/**
-	 * "DSAFD" stands for "Dead square and Freeze Deadlock"
-	 * This method is a help method to the method isDeadLock, it determines
-	 * whether the board is in a state when "Dead square deadlock" has occurred.
-	 * Please see: http://sokobano.de/wiki/index.php?title=Deadlocks#Dead_square_deadlocks
+	 * Actually the method name is misleading, "Wall" can either be a wall of
+	 * boxes (on goal or not) or a wall of Board.TYPE_WALL.
+	 * 
+	 * The method determines if the box is next to any wall
+	 * 
+	 * @param theBoard
+	 * @param boxPos
+	 * @return
+	 */
+	private boolean isNextToWall(Board theBoard, BoardCoordinate boxPos) {
+		//TODO is this necessary? Do in isBoxImmovable instead?
+		return true;
+	}
+
+	/**
+	 * This method determines whether the player can walk on a given square or
+	 * not.
+	 * 
+	 * @param squareData
+	 *            the square to examine
+	 * @return Returns FALSE if it is not reachable, else TRUE.
+	 */
+	private boolean isReachable(byte squareData) {
+		if (squareData == Board.TYPE_BOX
+				|| squareData == Board.TYPE_BOX_ON_GOAL
+				|| squareData == Board.TYPE_NOTHING
+				|| squareData == Board.TYPE_WALL) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * "DSAFD" stands for "Dead square and Freeze Deadlock" This method is a
+	 * help method to the method isDeadLock, it determines whether the board is
+	 * in a state when "Dead square deadlock" has occurred. Please see:
+	 * http://sokobano.de/wiki/index.php?title=Deadlocks#Dead_square_deadlocks
 	 * for further details and example.
 	 * 
 	 * Here comes heuristic for finding "Dead square deadlock":
 	 * 
-	 * Find position (coordinate) for all boxes and goals
-	 * Count non deadlocked boxes, if this count is less than
-	 * number of goals => deadlock. 
+	 * Find position (coordinate) for all boxes and goals Count non deadlocked
+	 * boxes, if this count is less than number of goals => deadlock.
 	 * 
 	 * This method uses a help method isBoxImmovable to see if it is deadlocked.
 	 * 
-	 * @param Board to determine if deadlocked or not.
+	 * @param Board
+	 *            to determine if deadlocked or not.
 	 * @returns "True" if the board is deadlocked.
 	 */
-	private boolean isDSAFD(Board pBoard) {
+	private boolean isDSAFD(Board theBoard) {
 		boolean isDead = true;
 
-		int i, j = 0;
+		byte i, j = 0;
 		int rowCount = 10; // TODO use method provided by Emil
 		int colCount = 10; // TODO use method provided by Emil
 
-		ArrayList<Pos> boxPositions = new ArrayList<Pos>();
-		ArrayList<Pos> goalPositions = new ArrayList<Pos>();
-		
-		//Get positions for every box and goal
-		for (i = 0; i < rowCount; i++) {
-			for(j = 0; j < colCount; j++) {
-				Pos currPos = new Pos(i,j);
-				byte currByte = pBoard.atPos(currPos);
+		ArrayList<BoardCoordinate> boxPositions = new ArrayList<BoardCoordinate>();
+		ArrayList<BoardCoordinate> goalPositions = new ArrayList<BoardCoordinate>();
 
-				//current position is a box
-				if(currByte == IS_BOX || currByte == IS_BOX_ON_GOAL) {
+		// Get positions for every box and goal
+		for (i = 0; i < rowCount; i++) {
+			for (j = 0; j < colCount; j++) {
+				BoardCoordinate currPos = new BoardCoordinate(i, j);
+				byte currByte = theBoard.dataAt(i, j);
+
+				// current position is a box
+				if (currByte == Board.TYPE_BOX
+						|| currByte == Board.TYPE_BOX_ON_GOAL) { // TODO low
+																	// prio,
+																	// when
+																	// merged,
+																	// remove
+																	// "Board."
 					boxPositions.add(currPos);
-				} else if(currByte == IS_GOAL) { //current position is a goal
+				} else if (currByte == Board.TYPE_GOAL_SQUARE) { // current
+																	// position
+																	// is a goal
 					goalPositions.add(currPos);
 				}
 			}
 		}
-		
-		//Count boxes and goals
+
+		// Count boxes and goals
 		int boxCount = boxPositions.size();
 		int goalCount = goalPositions.size();
-		
-		ArrayList<Pos> immovableBoxPositions = new ArrayList<Pos>();
-		
-		//Find immovable boxes, count them
-		for(i = 0; i < boxCount; i++) {
-			Pos currPos = boxPositions.get(i);
-			if(isBoxImmovable(currPos)) {
+
+		ArrayList<BoardCoordinate> immovableBoxPositions = new ArrayList<BoardCoordinate>();
+
+		// Find immovable boxes, count them
+		for (i = 0; i < boxCount; i++) {
+			BoardCoordinate currPos = boxPositions.get(i);
+			if (isBoxImmovable(theBoard, currPos)) {
 				immovableBoxPositions.add(currPos);
 			}
 		}
-		
+
 		int immovableBoxCount = immovableBoxPositions.size();
-		
-		if(immovableBoxCount > goalCount) {
+
+		if (immovableBoxCount > goalCount) {
 			isDead = true;
 		} else {
 			isDead = false;
 		}
-		
+
 		return isDead;
 	}
 
-
-	private boolean isCD(Board pBoard) {
+	private boolean isCD(Board theBoard) {
 		boolean isDead = true;
 		return isDead;
 	}
 
-	private boolean isBD(Board pBoard) {
+	private boolean isBD(Board theBoard) {
 		boolean isDead = true;
 		return isDead;
 	}
 
-	private boolean isDDTFB(Board pBoard) {
+	private boolean isDDTFB(Board theBoard) {
 		boolean isDead = true;
 		return isDead;
 	}
