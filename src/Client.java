b21c8dc3578f5d589f90ec53ad499b4b4aca6a61
Emil Arfvidsson <emil@arfvidsson.com>
2011-09-20 12:28:19 +0200

    Add Board, Solver, BoardState and modified Client.

--- src/Client.java
+++ src/Client.java
@@ -1,59 +1,48 @@
-import java.io.*;
-import java.net.*;
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Vector;
 
 public class Client {
 
-	public static void main(String[] pArgs) 
-	{
-		if(pArgs.length<3)
-		{
+	public static void main(String[] pArgs) {
+		if (pArgs.length < 3) {
 			System.out.println("usage: java Client host port boardnum");
 			return;
 		}
-	
-		try
-		{
-			Socket lSocket=new Socket(pArgs[0],Integer.parseInt(pArgs[1]));
-			PrintWriter lOut=new PrintWriter(lSocket.getOutputStream());
-			BufferedReader lIn=new BufferedReader(new InputStreamReader(lSocket.getInputStream()));
-	
-            lOut.println(pArgs[2]);
-            lOut.flush();
 
-            String lLine=lIn.readLine();
+		try {
+			Socket lSocket = new Socket(pArgs[0], Integer.parseInt(pArgs[1]));
+			PrintWriter lOut = new PrintWriter(lSocket.getOutputStream());
+			BufferedReader lIn = new BufferedReader(new InputStreamReader(lSocket.getInputStream()));
 
-            //read number of rows
-            int lNumRows=Integer.parseInt(lLine);
+			lOut.println(pArgs[2]);
+			lOut.flush();
 
-            //read each row
-            for(int i=0;i<lNumRows;i++)
-            {
-                lLine=lIn.readLine();
-                //here, we would store the row somewhere, to build our board
-                //in this demo, we just print it
-                System.out.println(lLine);
-            }
-    
-            //now, we should find a solution to the sokoban
+			String lLine = lIn.readLine();
 
-            //we've found our solution (this is actually the solution to board 1)    
-            String lMySol="U R R U U L D L L U L L D R R R R L D D R U R U D L L U R";
-            //these formats are also valid:
-            //String lMySol="URRUULDLLULLDRRRRLDDRURUDLLUR";
-            //String lMySol="0 3 3 0 0 2 1 2 2 0 2 2 1 3 3 3 3 2 1 1 3 0 3 0 1 2 2 0 3";
+			// read number of rows
+			int lNumRows = Integer.parseInt(lLine);
 
-            //send the solution to the server
-            lOut.println(lMySol);
-            lOut.flush();
-    
-            //read answer from the server
-            lLine=lIn.readLine();
-    
-            System.out.println(lLine);
-		}
-		catch(Throwable t)
-		{
+			// read each row
+			Vector<String> boardLines = new Vector<String>(lNumRows);
+			for (int i = 0; i < lNumRows; i++) {
+				lLine = lIn.readLine();
+				boardLines.add(lLine);
+			}
+			
+			Solver solver = new Solver();
+			String solution = solver.solve(boardLines);
+			
+			lOut.println(solution);
+			lOut.flush();
+
+			// read answer from the server
+			lLine = lIn.readLine();
+
+			System.out.println(lLine);
+		} catch (Throwable t) {
 			t.printStackTrace();
 		}
 	}
