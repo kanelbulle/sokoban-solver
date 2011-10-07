9b4c7fb68d388c1fe25a0696ec0ab34c7dbca81e
Emil Arfvidsson <emil@arfvidsson.com>
2011-10-07 12:47:16 +0200

    Add board test loop in client

--- src/Client.java
+++ src/Client.java
@@ -6,57 +6,54 @@ import java.util.Vector;
 
 public class Client {
 
-	/*
-
-########
-#   # .#
-#   $$.#
-####   #
-   #@ ##
-   #### 
-
-
-	 */
-	
 	public static void main(String[] pArgs) {
 		if (pArgs.length < 3) {
 			System.out.println("usage: java Client host port boardnum");
 			return;
 		}
 
-		try {
-			Socket lSocket = new Socket(pArgs[0], Integer.parseInt(pArgs[1]));
-			PrintWriter lOut = new PrintWriter(lSocket.getOutputStream());
-			BufferedReader lIn = new BufferedReader(new InputStreamReader(lSocket.getInputStream()));
+		pArgs[0] = "130.237.218.85";
+		pArgs[1] = "7781";
 
-			lOut.println(pArgs[2]);
-			lOut.flush();
+		for (int n = 1; n < 136; n++) {
+			pArgs[2] = "" + n;
+			System.out.println("Trying board " + n);
+			try {
+				Socket lSocket = new Socket(pArgs[0],
+						Integer.parseInt(pArgs[1]));
+				PrintWriter lOut = new PrintWriter(lSocket.getOutputStream());
+				BufferedReader lIn = new BufferedReader(new InputStreamReader(
+						lSocket.getInputStream()));
 
-			String lLine = lIn.readLine();
-			// read number of rows
-			int lNumRows = Integer.parseInt(lLine);
+				lOut.println(pArgs[2]);
+				lOut.flush();
 
-			// read each row
-			Vector<String> boardLines = new Vector<String>(lNumRows);
-			for (int i = 0; i < lNumRows; i++) {
+				String lLine = lIn.readLine();
+				// read number of rows
+				int lNumRows = Integer.parseInt(lLine);
+
+				// read each row
+				Vector<String> boardLines = new Vector<String>(lNumRows);
+				for (int i = 0; i < lNumRows; i++) {
+					lLine = lIn.readLine();
+					boardLines.add(lLine);
+				}
+
+				Solver solver = new Solver();
+				Board board = new Board(boardLines);
+
+				String solution = solver.solve(board);
+
+				lOut.println(solution);
+				lOut.flush();
+
+				// read answer from the server
 				lLine = lIn.readLine();
-				boardLines.add(lLine);
+
+				System.out.println(lLine);
+			} catch (Throwable t) {
+				t.printStackTrace();
 			}
-			
-			Solver solver = new Solver();
-			Board board = new Board(boardLines);
-			
-			String solution = solver.solve(board);
-			
-			lOut.println(solution);
-			lOut.flush();
-
-			// read answer from the server
-			lLine = lIn.readLine();
-
-			System.out.println(lLine);
-		} catch (Throwable t) {
-			t.printStackTrace();
 		}
 	}
 }
