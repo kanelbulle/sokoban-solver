a75a5581d12b7521a4dcb4d410b3cfe7a7986162
Emil Arfvidsson <emil@arfvidsson.com>
2011-10-13 19:47:44 +0200

    Add make and readme, fixed script

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 zip:
-	zip sokoban-solver.zip compileAndRun.sh src/*.java
+	cp -r src/ code/
+	zip sokoban-solver-E-SEA.zip compileAndRun.sh README.txt code/ report-ai10-groupE-SEA.pdf
+	rm -r code/
--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd src
-javac *.java
-java Client 130.237.218.85 7777 1
+EXPECTED_ARGS=2
+E_BADARGS=65
+
+if [ $# -ne $EXPECTED_ARGS ]; then
+	echo "Usage: `basename $0` {port} {board}"
+	exit $E_BADARGS
+else
+	cd code
+	javac *.java
+
+	java Client 130.237.218.85 $1 $2
+fi
